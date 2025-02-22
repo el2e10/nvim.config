873b5cccc4e1abda621e8475bcb69bf443a0689a
el2e10 <eldhoittangeorge@gmail.com>
2025-02-22 18:37:35 +0530

    Restart auto completion

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -23,7 +23,7 @@ return {
 			lspconfig.ruff.setup {}
 			lspconfig.pyright.setup {}
 			lspconfig.clangd.setup {}
-			lspconfig.zls.setup{}
+			lspconfig.zls.setup {}
 			lspconfig.biome.setup {
 				filetypes = { 'javascript', 'typescript', 'json' },
 			}
@@ -53,31 +53,14 @@ return {
 			{
 				'L3MON4D3/LuaSnip',
 				build = (function()
-					-- Build Step is needed for regex support in snippets.
-					-- This step is not supported in many windows environments.
-					-- Remove the below condition to re-enable on windows.
 					if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
 						return
 					end
 					return 'make install_jsregexp'
 				end)(),
-				dependencies = {
-					-- `friendly-snippets` contains a variety of premade snippets.
-					--    See the README about individual language/framework/plugin snippets:
-					--    https://github.com/rafamadriz/friendly-snippets
-					-- {
-					--   'rafamadriz/friendly-snippets',
-					--   config = function()
-					--     require('luasnip.loaders.from_vscode').lazy_load()
-					--   end,
-					-- },
-				},
+				dependencies = {},
 			},
 			'saadparwaiz1/cmp_luasnip',
-
-			-- Adds other completion capabilities.
-			--  nvim-cmp does not ship with all sources by default. They are split
-			--  into multiple repos for maintenance purposes.
 			'hrsh7th/cmp-nvim-lsp',
 			'hrsh7th/cmp-path',
 		},
@@ -93,7 +76,7 @@ return {
 						luasnip.lsp_expand(args.body)
 					end,
 				},
-				completion = { completeopt = 'menu,menuone,noinsert', autocomplete = false },
+				completion = { completeopt = 'menu,menuone,noinsert' },
 
 				-- For an understanding of why these mappings were
 				-- chosen, you will need to read `:help ins-completion`
